d622e97f984b68a63c112f881b2aec81f51af167
Sahil Satyam <54464202+SahilSatyam@users.noreply.github.com>
2023-10-05 20:29:52 +0530

    Update Text.py
    New

--- Text.py
+++ Text.py
@@ -19,3 +19,23 @@ model.add(GRU(units=32, return_sequences=True))    # Add more GRU layers if need
 
 # Compile the model and specify loss, optimizer, and metrics
 model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
+
+
+new 
+from keras.models import Sequential
+from keras.layers import Embedding, LSTM, Dense
+
+# Create a Sequential model
+model = Sequential()
+
+# Add an Embedding layer (as discussed earlier)
+model.add(Embedding(input_dim=vocab_size, output_dim=embedding_dim, input_length=max_sequence_length))
+
+# Add LSTM or GRU layers (choose one, you can also stack more if needed)
+model.add(LSTM(units=64, return_sequences=True))
+
+# Add a Dense layer for prediction with softmax activation
+model.add(Dense(units=vocab_size, activation='softmax'))
+
+# Compile the model and specify loss, optimizer, and metrics
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
