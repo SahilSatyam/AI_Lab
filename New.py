70277d62f638722d77f6750758bf993a39cc6598
Sahil Satyam <54464202+SahilSatyam@users.noreply.github.com>
2023-10-27 18:53:32 +0530

    Update New.py

--- New.py
+++ New.py
@@ -1,80 +1,37 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from transformers import BertTokenizer, BertForQuestionAnswering, TrainingArguments, Trainer
-import torch
+To describe your accomplishments and delivery for the full year in your year-end review, you should focus on both the "what" (results) and the "how" (behaviors) of your work. Here's a breakdown of your accomplishments and delivery for the year based on the tasks you've mentioned:
 
-# Step 1: Data Preparation
+**1) KT Sessions and Training**
+- **What**: Successfully completed knowledge transfer sessions and training.
+- **How**: Actively engaged in learning and absorbing new information, demonstrating a commitment to self-improvement.
 
-# Load your Excel data into a pandas DataFrame
-df = pd.read_excel("your_data.xlsx")
+**2) Ingestions for Metamart Decommission**
+- **What**: Efficiently handled data ingestions from various sources.
+- **How**: Demonstrated strong data management and integration skills, ensuring data was accurately and securely transferred.
 
-# Preprocess the data (you may need to clean and preprocess text)
-# In this example, we assume you have a 'Description' column and a 'Closing steps' column
-# Concatenate them into a 'context' column for QA
-df['context'] = df['Description'] + " " + df['Closing steps']
+**3) Erwin Data Modeling**
+- **What**: Effectively performed data modeling for each of the ingestions.
+- **How**: Demonstrated proficiency in data modeling tools, ensuring data structures were well-defined and optimized.
 
-# Split the data into a training set and a testing set
-train_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
+**4) Contract Testing for OpenAPI Ingestions**
+- **What**: Successfully conducted contract testing for OpenAPI ingestions.
+- **How**: Paid attention to detail and thorough testing to ensure data quality and integrity.
 
-# Step 2: Model Training
+**5) Creating Reporting Logic for Database Growth Trend Dashboard**
+- **What**: Developed a database growth trend dashboard with accurate reporting logic.
+- **How**: Demonstrated strong analytical and data visualization skills, creating a valuable resource for the team.
 
-# Load BERT-Medium model and tokenizer
-model_name = "bert-medium-cased"
-tokenizer = BertTokenizer.from_pretrained(model_name)
-model = BertForQuestionAnswering.from_pretrained(model_name)
+**6) Developing Front End using React and MDS Components**
+- **What**: Created a user-friendly front end for ingestion metrics and report generation.
+- **How**: Showed proficiency in web development, making data more accessible and interactive for the team.
 
-# Define a function to prepare your data for training
-def prepare_qa_data(examples):
-    inputs = tokenizer(examples['question'], examples['context'], truncation=True, padding='max_length', max_length=512)
-    targets = {}
-    targets["start_positions"] = examples['start_positions']
-    targets["end_positions"] = examples['end_positions']
-    return inputs, targets
+**7) Developing Front End for Decom Dashboard Page**
+- **What**: Designed a front end for the Decom Dashboard.
+- **How**: Showed creative and technical skills in presenting data and information in a user-friendly way.
 
-# Format your training and testing data
-train_data = train_df.apply(prepare_qa_data, axis=1)
-test_data = test_df.apply(prepare_qa_data, axis=1)
+**8) Developing Front End for Database Growth Trend Page**
+- **What**: Designed a front end for the Database Growth Trend page.
+- **How**: Demonstrated consistency in creating user-friendly interfaces, enhancing the team's data analysis capabilities.
 
-# Define your training arguments
-training_args = TrainingArguments(
-    output_dir="./qa_model",
-    per_device_train_batch_size=8,
-    num_train_epochs=3,
-    evaluation_strategy="epoch",
-    save_total_limit=2,
-    load_best_model_at_end=True,
-)
+In the context of the environment for the year, consider any additional factors or challenges that you faced, such as changing project requirements, tight deadlines, or any other factors that influenced your work. For example, you could mention how you adapted to evolving project needs, worked effectively under pressure, or collaborated with team members to overcome obstacles.
 
-# Create a Trainer
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=train_data.tolist(),
-    eval_dataset=test_data.tolist(),
-)
-
-# Train the model
-trainer.train()
-
-# Save the trained model
-trainer.save_model()
-
-# Step 3: Question-Answering Interface
-
-# Load the fine-tuned model
-qa_model = BertForQuestionAnswering.from_pretrained("./qa_model")
-
-# Create a function to perform question-answering
-def ask_question(query, context):
-    inputs = tokenizer.encode_plus(query, context, add_special_tokens=True, return_tensors="pt")
-    start_positions, end_positions = qa_model(**inputs)
-    answer_start = torch.argmax(start_positions)
-    answer_end = torch.argmax(end_positions) + 1
-    answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(inputs["input_ids"][0][answer_start:answer_end]))
-    return answer
-
-# Example usage:
-query = "What should I do when I encounter this issue?"
-context = "Description of the issue goes here... Closing steps go here..."
-closing_steps = ask_question(query, context)
-print("Closing Steps:", closing_steps)
+Overall, your year-end review should showcase your contributions and highlight your specific achievements, emphasizing the "what" and "how" to demonstrate your skills, dedication, and value to the firm.
