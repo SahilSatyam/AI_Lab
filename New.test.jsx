927098ca041ecc0d29a61d1a8b95ea56522c1733
Sahil Satyam <54464202+SahilSatyam@users.noreply.github.com>
2023-11-08 01:11:09 +0530

    Update New.test.jsx

--- New.test.jsx
+++ New.test.jsx
@@ -1,68 +1,29 @@
-import React from 'react';
-import { render, fireEvent, waitFor } from '@testing-library/react';
-import DBLevelTrend from './DBLevelTrend';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
 
-describe('DBLevelTrend Component', () => {
-  it('should render the component', () => {
-    const { getByTestId } = render(<DBLevelTrend />);
-    const dbPage = getByTestId('db-page');
-    expect(dbPage).toBeInTheDocument();
-  });
+test('should display data in DB Level Page', () => {
+  const mockData = {
+    // Your mock data here
+  };
 
-  it('should fetch and display data', async () => {
-    // Mock the fetch function and response data
-    global.fetch = jest.fn().mockResolvedValue({
-      json: jest.fn().mockResolvedValue([]),
-    });
+  // Render the AppLevelTrend within a MemoryRouter
+  render(
+    <MemoryRouter initialEntries={['/app-level-trend']}>
+      <Routes>
+        <Route path="/app-level-trend" element={<AppLevelTrend />} />
+        <Route path="/db-level-trend-dashboard" element={<DBLevelTrend />} />
+      </Routes>
+    </MemoryRouter>
+  );
 
-    const { getByTestId, queryByText } = render(<DBLevelTrend />);
-    await waitFor(() => {
-      expect(queryByText('No Data Found')).toBeNull();
-    });
-  });
+  // Simulate the navigation to DB Level Page (e.g., by clicking a button)
+  const moreDetailsButton = screen.getByText('DB Level Details');
+  userEvent.click(moreDetailsButton);
 
-  it('should handle filtering based on input values', async () => {
-    const { getByPlaceholderText, queryByText } = render(<DBLevelTrend />);
-    const dbIDInput = getByPlaceholderText('Search By Databse ID');
-    const dbNameInput = getByPlaceholderText('Search By Database Name');
+  // Check if the data is displayed in the DBLevelTrend component
+  const dataElement = screen.getByText('Your Data Label'); // Replace with a label or content specific to your data
 
-    fireEvent.change(dbIDInput, { target: { value: '123' } });
-    fireEvent.change(dbNameInput, { target: { value: 'DB' } });
-
-    await waitFor(() => {
-      // Assert the filtered data is displayed correctly
-      expect(queryByText('Filtered Data')).toBeInTheDocument();
-    });
-  });
-
-  it('should show chart on "View Chart" click', async () => {
-    // Mock the fetch function and response data
-    global.fetch = jest.fn().mockResolvedValue({
-      json: jest.fn().mockResolvedValue({ rows: [], columns: [] }),
-    });
-
-    const { getByText, getByTestId } = render(<DBLevelTrend />);
-    const viewChartButton = getByText('View Chart');
-
-    fireEvent.click(viewChartButton);
-
-    await waitFor(() => {
-      const lineChart = getByTestId('mds-line-chart');
-      expect(lineChart).toBeInTheDocument();
-    });
-  });
-
-  it('should show spinner during data fetching', async () => {
-    global.fetch = jest.fn().mockResolvedValue(new Promise(() => {}));
-    const { getByTestId } = render(<DBLevelTrend />);
-    await waitFor(() => {
-      const spinner = getByTestId('mds-progress-spinner');
-      expect(spinner).toBeInTheDocument();
-    });
-  });
-
-  it('should match snapshot', () => {
-    const { asFragment } = render(<DBLevelTrend />);
-    expect(asFragment()).toMatchSnapshot();
-  });
+  // Assert that the data is present in the DBLevelTrend component
+  expect(dataElement).toBeInTheDocument();
 });
