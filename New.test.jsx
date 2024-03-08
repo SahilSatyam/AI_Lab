4c828f6ce6f26b8dc0d5bd7b7a4d3ea0e9d06eba
Sahil Satyam <54464202+SahilSatyam@users.noreply.github.com>
2024-03-08 15:55:10 +0530

    Update New.test.jsx

--- New.test.jsx
+++ New.test.jsx
@@ -1,26 +1,38 @@
-from atlassian import Jira
+def create_issue(self, fields, update_history=False, update=None):
+        """
+        Creates an issue or a sub-task from a JSON representation
+        :param fields: JSON data
+                mandatory keys are issuetype, summary and project
+        :param update: JSON data
+                Use it to link issues or update worklog
+        :param update_history: bool (if true then the user's project history is updated)
+        :return:
+            example:
+                fields = dict(summary='Into The Night',
+                              project = dict(key='APA'),
+                              issuetype = dict(name='Story')
+                              )
+                update = dict(issuelinks={
+                    "add": {
+                        "type": {
+                            "name": "Child-Issue"
+                            },
+                        "inwardIssue": {
+                            "key": "ISSUE-KEY"
+                            }
+                        }
+                    }
+                )
+                jira.create_issue(fields=fields, update=update)
+        """
+        url = self.resource_url("issue")
+        data = {"fields": fields}
+        if update:
+            data["update"] = update
+        params = {}
 
-jira_url = "https://jiradc-ccb-cluster02.prod.aws.jpmchase.net"
-jira_api_token = 'UjczMjEzMjptby1hcGktblJuUFUzNTQ2U1I3OXdMR1FRUXJSaTBn'
-
-try:
-    logging.basicConfig(level=logging.DEBUG)
-    jira = Jira(jira_url, token=f"Bearer {jira_api_token}")
-    print(f"Jira API version: {jira.api_version}")
-
-    # Get project key or project ID
-    project_key = "YOUR_PROJECT_KEY"
-    # project_id = "YOUR_PROJECT_ID"
-
-    # Retrieve issues for the project
-    issues = jira.project_issues(project_key, expand='renderedFields')
-
-    # Iterate over issues
-    for issue in issues:
-        print(f"Issue Key: {issue.key}, Summary: {issue.fields.summary}, Status: {issue.fields.status.name}")
-
-    return 'Jira connected successfully', 200
-except Exception as e:
-    current_app.logger.error(f"Error while connecting to Jira: {str(e)}")
-    traceback.print_exc()
-    return "Failed to connect to Jira", 400
+        if update_history is True:
+            params["updateHistory"] = "true"
+        else:
+            params["updateHistory"] = "false"
+        return self.post(url, params=params, data=data)
