b3dd2db1ef7e9f7bfb5897a0eda1dd02424bf546
Sahil Satyam <54464202+SahilSatyam@users.noreply.github.com>
2024-03-08 16:31:41 +0530

    Update New.test.jsx

--- New.test.jsx
+++ New.test.jsx
@@ -1,38 +1,9 @@
-def create_issue(self, fields, update_history=False, update=None):
-        """
-        Creates an issue or a sub-task from a JSON representation
-        :param fields: JSON data
-                mandatory keys are issuetype, summary and project
-        :param update: JSON data
-                Use it to link issues or update worklog
-        :param update_history: bool (if true then the user's project history is updated)
-        :return:
-            example:
-                fields = dict(summary='Into The Night',
-                              project = dict(key='APA'),
-                              issuetype = dict(name='Story')
-                              )
-                update = dict(issuelinks={
-                    "add": {
-                        "type": {
-                            "name": "Child-Issue"
-                            },
-                        "inwardIssue": {
-                            "key": "ISSUE-KEY"
-                            }
-                        }
-                    }
-                )
-                jira.create_issue(fields=fields, update=update)
-        """
-        url = self.resource_url("issue")
-        data = {"fields": fields}
-        if update:
-            data["update"] = update
-        params = {}
+issue_fields = {
+    "project": {"key": "YOUR_PROJECT_KEY"},
+    "summary": "New issue summary",
+    "description": "This is a new issue description",
+    "issuetype": {"name": "Task"}
+}
 
-        if update_history is True:
-            params["updateHistory"] = "true"
-        else:
-            params["updateHistory"] = "false"
-        return self.post(url, params=params, data=data)
+# Create the new issue
+new_issue = jira.create_issue(fields=issue_fields)
